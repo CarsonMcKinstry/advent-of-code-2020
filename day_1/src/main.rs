47df8390428c694698cb909c7a1459ec6a47e059
Carson McKinstry <carson.mckinstry@trivago.com>
2020-12-01 15:04:53 +0100

    day 1, task 2

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -1,9 +1,8 @@
 use std::env;
 use std::fs;
 
-fn task_1(ledger_lines: Vec<u32>) -> u32 {
-
-    let mut total: u32 = 0;
+fn task_1(ledger_lines: &Vec<u32>) -> u32 {
+    let mut answer: u32 = 0;
 
     for (i, line) in ledger_lines.iter().enumerate() {
         let lines_to_check = &ledger_lines[i + 1..];
@@ -11,13 +10,35 @@ fn task_1(ledger_lines: Vec<u32>) -> u32 {
         if lines_to_check.len() > 0 {
             for line_to_check in lines_to_check {
                 if line + line_to_check == 2020 {
-                    total = line * line_to_check;
+                    answer = line * line_to_check;
                 }
             }
         }
     }
 
-    total
+    answer
+}
+
+fn task_2(ledger_lines: &Vec<u32>) -> u32 {
+    let mut answer: u32 = 0;
+
+    for (i, line) in ledger_lines.iter().enumerate() {
+        let lines_to_check = &ledger_lines[i + 1..];
+
+        if lines_to_check.len() > 0 {
+            for (j, line_to_check) in lines_to_check.iter().enumerate() {
+                let lines_to_check_2 = &lines_to_check[j + 1..];
+
+                for line_to_check_2 in lines_to_check_2 {
+                    if line + line_to_check + line_to_check_2 == 2020 {
+                        answer = line * line_to_check * line_to_check_2;
+                    }
+                }
+            }
+        }
+    }
+
+    answer
 }
 
 fn main() {
@@ -33,5 +54,26 @@ fn main() {
         .map(|n| n.parse::<u32>().unwrap())
         .collect::<Vec<u32>>();
 
-    println!("Task 1: {}", task_1(ledger_lines));
+    println!("Task 1: {}", task_1(&ledger_lines));
+    println!("Task 2: {}", task_2(&ledger_lines));
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn task_1_returns_correct_value() {
+        let ledger_lines: Vec<u32> = vec![1721, 979, 366, 299, 675, 1456];
+        let result = task_1(&ledger_lines);
+        assert_eq!(result, 514579);
+    }
+
+    #[test]
+    fn task_2_returns_correct_value() {
+        let ledger_lines: Vec<u32> = vec![1721, 979, 366, 299, 675, 1456];
+        let result = task_2(&ledger_lines);
+        assert_eq!(result, 241861950);
+    }
 }
